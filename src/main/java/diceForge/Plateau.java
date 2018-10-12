44ce3bd9d696a61ff8e8daf24740b4ac58b93cc9
GabrielPlante <gabriel.plante@live.fr>
2018-10-12 14:16:05 +0200

    Changement de notation

--- src/main/java/diceForge/Plateau.java
+++ src/main/java/diceForge/Plateau.java
@@ -1,16 +1,5 @@
 package diceForge;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class Plateau {
-    private Bassin[] piscine;
-    public Plateau(){//C'est ici qu'on défini ce qu'il va y avoir dans le plateau
-        piscine = new Bassin[2];//Pour la version minimale, il n'y a que 2 bassins
-        Face or = new Face(new Ressource[][]{{new Or(), new Or()}});//On initialise la face or
-        Face soleil = new Face(new Ressource[][]{{ new Soleil()}});//Et la face soleil
-        piscine[0] = new Bassin(2, or, 2);//Et on créé chaque bassins avec leur face
-        piscine[1] = new Bassin(2, soleil, 1);
-    }
+public class Plateau {//C'est ici qu'on défini ce qu'il va y avoir dans le plateau
+    private Temple temple = new Temple();
 }
--- src/main/java/diceForge/Ressource.java
+++ src/main/java/diceForge/Ressource.java
@@ -1,4 +1,9 @@
 package diceForge;
 
+/*Parce qu'il va y avoir des faces de dé complexes,
+et aussi pour avoir un unique tableau dans Face,
+on créé une classe Ressource que l'on dérive en chaque ressource de base,
+mais aussi (plus tard) en face de dé complexe
+ */
 public class Ressource {
 }
