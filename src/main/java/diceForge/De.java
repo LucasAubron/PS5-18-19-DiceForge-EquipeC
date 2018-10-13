6688776d72147370e0a0993fb2b9f0fcb63c0710
GabrielPlante <gabriel.plante@live.fr>
2018-10-13 11:05:40 +0200

    1ere itération du dé

---
+++ src/main/java/diceForge/De.java
@@ -0,0 +1,32 @@
+package diceForge;
+
+import java.util.Random;
+
+public class De {
+    /**
+     * La classe représentant un dé (oui De c'est pas très intuitif, si quelqu'un a mieux il le refactor)
+     */
+    private Face[] faces;
+
+    public Face[] getFaces() {
+        return faces;
+    }
+
+    public De(Face[] faces){
+        if (faces.length != 3)//Pour la version minimale, le dé à 3 faces
+            throw new RuntimeException("Le nombre de face est invalide. Attendu : 3, actuel : "+faces.length);
+        this.faces = faces;
+    }
+
+    public Face lancerLeDe(){
+        Random aleatoire = new Random();//Permet d'acceder au fonction de Random
+        int face = aleatoire.nextInt(faces.length);//Nombre entre 0 et faces.length-1
+        return faces[face];
+    }
+
+    public void forger(Face faceAForger, int numFace){
+        if (numFace < 0 || numFace > faces.length-1)
+            throw new RuntimeException("Le numéro de la face est invalide. Min : 1, max : "+(faces.length-1)+", actuel : "+numFace);
+        faces[numFace] = faceAForger;
+    }
+}
