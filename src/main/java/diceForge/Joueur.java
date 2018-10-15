ee48e4b91e0ee4f821bb6214636f693fcf036d85
Lucas Aubron <lucas.aubron@etu.unice.fr>
2018-10-15 15:12:51 +0200

    affichage mode verbeux plus compacte

--- src/main/java/diceForge/Joueur.java
+++ src/main/java/diceForge/Joueur.java
@@ -94,7 +94,7 @@ public abstract class Joueur {
 
     public String printRessourcesEtDes(int numeroManche){
         String res = "Manche: " + numeroManche + " || Joueur: " + identifiant + "\n" ;
-        res += "Res 1er dé: " + premierDeFaceCourante.toString() + "\t||\t" + "Res 2ème dé: Non implémenté en version minimale\n";
+        res += "Res 1er dé: " + premierDeFaceCourante.toString() + "\t||\t" + "Res 2ème dé: /\n";
         res += "Or: " + or + "\t||\t" + "Soleil: " + soleil + "\t||\t" + "Lune: /" + "\t||\t" + "PointDeGloire: " + pointDeGloire + "\n";
         res += "--------------------------------------------------------------------------------------------------";
         return res;
