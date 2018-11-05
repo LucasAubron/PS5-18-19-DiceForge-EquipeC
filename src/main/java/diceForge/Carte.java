c13640627f5e72bd885f66c91ee7479e33079653
GabrielPlante <gabriel.plante@live.fr>
2018-11-05 14:29:16 +0100

    Listes des cartes (incompletes)

--- src/main/java/diceForge/Carte.java
+++ src/main/java/diceForge/Carte.java
@@ -3,6 +3,10 @@ package diceForge;
 /**
  * La classe carte peut être utilisé pour les cartes ne donnant que des points de gloire
  * Sinon il faut créer / utiliser une classe dérivée de celle ci
+ * Nom des cartes:
+ * Coffre ; Herbes folles ; Ancien ; Marteau ; Ours ; Sanglier ; Biche ; Satyres
+ * Hibou ; Minautore ; Bateau celeste ; Bouclier ; Cerberes ; Passeur ; Casque d invisibilite
+ * Gorgone ; Triton ; Abysse
  */
 public class Carte {
     private Ressource[] cout;
