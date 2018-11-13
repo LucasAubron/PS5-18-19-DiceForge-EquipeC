9e4c1ce0cbf31fd3ee47023c1a29e8dad697fa16
Lucas Aubron <lucas.aubron@etu.unice.fr>
2018-11-13 21:36:49 +0100

    c'est l'heure du duel, création du bot aubron

---
+++ src/main/java/bot/AubotLeGrand.java
@@ -0,0 +1,90 @@
+package bot;
+
+import diceForge.*;
+
+import java.util.List;
+
+public class AubotLeGrand extends Joueur{
+    public AubotLeGrand(int identifiant, Afficheur afficheur){ super(identifiant, afficheur); }
+
+    @Override
+    public Joueur.Action choisirAction(int numManche){
+        return null;
+    }
+
+    @Override
+    public ChoixJoueurForge choisirFaceAForgerEtARemplacer(List<Bassin> bassins, int numManche){
+        return null;
+    }
+
+    @Override
+    public Carte choisirCarte(List<Carte> cartes, int numManche){
+        return null;
+    }
+
+    @Override
+    public boolean choisirActionSupplementaire(int numManche){
+        return true;
+    }
+
+    @Override
+    public int choisirRepartitionOrMarteau(int quantiteOr){
+        return 0;
+    }
+
+    @Override
+    public List<Joueur.Renfort> choisirRenforts(List<Joueur.Renfort> renforts){
+        return null;
+    }
+
+    @Override
+    public int choisirRessource(Face face){
+        return 0;
+    }
+
+    @Override
+    public int choisirRessourceAPerdre(Face face){
+        return 0;
+    }
+
+    @Override
+    public int choisirDeFaveurMineure(){
+        return 0;
+    }
+
+    @Override
+    public int choisirDeCyclope(){
+        return 0;
+    }
+
+    @Override
+    public int choisirIdJoueurPorteurSanglier(List<Joueur> joueurs){
+        return 0;
+    }
+
+    @Override
+    public void forgerFace(Face face){
+    }
+
+    @Override
+    public int choisirFacePourGagnerRessource(List<Face> faces){
+        return 0;
+    }
+
+    @Override
+    public Joueur.choixJetonTriton utiliserJetonTriton(){
+        return null;
+    }
+
+    @Override
+    public boolean utiliserJetonCerbere(){
+        return true;
+    }
+
+    @Override
+    public boolean choisirRessourceOuPdg(Ressource ressource) {
+        return true;
+    }
+}
+
+}
