535c767a1ea38a9a7f21c0ce5eb13064b96011cc
RaphaelRomanet <raphael.romanet@gmail.com>
2018-11-13 13:47:27 +0100

    Ajout de la classe de test de portailOriginel

--- src/test/java/diceForge/PortailsOriginelsTest.java
+++ src/test/java/diceForge/PortailsOriginelsTest.java
@@ -1,26 +1,43 @@
 package diceForge;
 
+
+import bot.RandomBot;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PortailsOriginelsTest {
-    /*
-    private Joueur j0 = new RandomBot(0);
-    private Joueur j1 = new RandomBot(1);
-    private PortailsOriginels p0 = new PortailsOriginels(new Joueur[]{j0, j1}, false);
+
+    private Joueur j0 = new RandomBot(1,new Afficheur(false));
+    private Joueur j1 = new RandomBot(2,new Afficheur(false));
+    
+    private PortailsOriginels p0 = new PortailsOriginels(new Joueur.Bot[]{Joueur.Bot.EasyBot,Joueur.Bot.RandomBot,Joueur.Bot.EasyBot,Joueur.Bot.RandomBot}, new Afficheur(false));
 
     @Test
     public void retirerJoueur() {
         assertEquals(p0.getJoueurs().size(), 2);
-        assertEquals(p0.retirerJoueur(0).getIdentifiant(), j0.getIdentifiant());
+        assertEquals(p0.retirerJoueur(1).getIdentifiant(), j0.getIdentifiant());
         assertEquals(p0.getJoueurs().size(), 1);
-        assertEquals(p0.retirerJoueur(1).getIdentifiant(), j1.getIdentifiant());
+        assertEquals(p0.retirerJoueur(2).getIdentifiant(), j1.getIdentifiant());
+        assertEquals(p0.getJoueurs().size(), 0);
+        p0.ajouterJoueur(j1);
+        assertEquals(p0.getJoueurs().size(), 1);
+        p0.ajouterJoueur(j0);
+        assertEquals(p0.getJoueurs().size(),2);
+
+    }
+
+    @Test
+    public void ajouterJoueur() {
+        assertEquals(p0.getJoueurs().size(), 2);
+        assertEquals(p0.retirerJoueur(1).getIdentifiant(), j0.getIdentifiant());
+        assertEquals(p0.getJoueurs().size(), 1);
+        assertEquals(p0.retirerJoueur(2).getIdentifiant(), j1.getIdentifiant());
         assertEquals(p0.getJoueurs().size(), 0);
         p0.ajouterJoueur(j1);
         assertEquals(p0.getJoueurs().size(), 1);
         p0.ajouterJoueur(j0);
         assertEquals(p0.getJoueurs().size(),2);
     }
-    */
+
 }
