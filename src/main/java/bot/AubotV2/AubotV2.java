324a2d2d60fb441d59238b518f44aacfe39954f7
Lucas Aubron <lucas.aubron@etu.unice.fr>
2018-11-17 19:46:46 +0100

    new Aubot

---
+++ src/main/java/bot/AubotV2/AubotV2.java
@@ -0,0 +1,235 @@
+package bot.AubotV2;
+
+import diceForge.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import static diceForge.Carte.Noms.*;
+import static diceForge.Carte.Noms.Bouclier;
+
+public class AubotV2 extends Joueur{
+    private int nombreDeLancerParManche;
+    private Random random;
+    private boolean secondeAction = false;
+    private boolean desComplet;
+    private int nombreDeJoueurs;
+    private int manche = 0;
+    private int compteurForge;
+    private int orManquant;
+    private int luneManquant;
+    private int soleilManquant;
+
+
+    //--------------------------------------------
+    private int orDe = 11;
+    private int soleilDe = 3;
+    private int luneDe = 2;
+    //--------------------------------------------
+
+    /**
+     * @param identifiant comprit entre 1 et 4 inclus
+     * @param afficheur
+     * @param plateau
+     */
+    public AubotV2(int identifiant, Afficheur afficheur, Plateau plateau) {
+        super(identifiant, afficheur, plateau);
+        compteurForge = 0;
+        desComplet = false;
+        random = new Random();
+    }
+
+    @Override
+    public Joueur.Action choisirAction(int numManche) {
+        if (manche == 0) {//on fait ici ce qu'on n'a pas pu faire dans le constructeur (du fait que le plateau n'est pas encore complètement initialisé à ce moment là)
+            int indice = 0;
+            nombreDeJoueurs = getPlateau().getJoueurs().size();
+            nombreDeLancerParManche = nombreDeJoueurs ==3  ? 3:4;
+        }
+        manche++;
+        statsDe();
+        return Action.FORGER;
+    }
+
+    @Override
+    public ChoixJoueurForge choisirFaceAForgerEtARemplacer(List<Bassin> bassins, int numManche){
+        Bassin bassin = bassins.get(0);
+        int numFace = 0;
+        int numDe = 0;
+        int posFace = 0;
+        if (bassins.isEmpty())
+            return new ChoixJoueurForge(null, 0, 0, 0);
+        return new ChoixJoueurForge(bassin, numFace, numDe, posFace);
+    }
+
+    @Override
+    public Carte choisirCarte(List<Carte> cartes, int numManche){
+        return cartes.get(0);
+    }
+
+
+    @Override
+    public int choisirRepartitionOrMarteau(int quantiteOr){
+        return 0;
+    }
+
+    @Override
+    public boolean choisirActionSupplementaire(int numManche){
+        return false;
+    }
+
+    @Override
+    public List<Renfort> choisirRenforts(List renfortsUtilisables){
+        return renfortsUtilisables;
+    }
+
+    @Override
+    public int choisirRessource(Face faceAChoix){
+        return 0;
+    }
+
+    @Override
+    public int choisirRessourceAPerdre(Face faceAChoix){
+        return 0;
+    }
+
+    @Override
+    public int choisirDeFaveurMineure(){
+        return 0;
+    }
+
+    @Override
+    public int choisirDeCyclope(){
+        return 0;
+    }
+
+    @Override
+    public int choisirIdJoueurPorteurSanglier(List<Joueur> joueurs) {
+        return (getIdentifiant() == 1 ? 2 : 1);
+    }
+
+    @Override
+    public void forgerFace(Face face){
+        forgerDe(0, face, 0);
+    }
+
+
+    @Override
+    public int choisirFacePourGagnerRessource(List<Face> faces){
+        return 0;
+    }
+
+    @Override
+    public choixJetonTriton utiliserJetonTriton(){
+        Random random = new Random();
+        int choix = random.nextInt(choixJetonTriton.values().length);
+        switch (choix){
+            case 0:
+                return choixJetonTriton.Rien;
+            case 1:
+                return choixJetonTriton.Or;
+            case 2:
+                return choixJetonTriton.Soleil;
+            case 3:
+                return choixJetonTriton.Lune;
+        }
+        throw new DiceForgeException("Bot","Impossible, utiliserJetonTriton ne renvoit rien !!");
+    }
+
+    @Override
+    public boolean utiliserJetonCerbere(){
+        return true;
+    }
+
+    @Override
+    public boolean choisirRessourceOuPdg(Ressource ressource){
+        return true;
+    }
+
+    @Override
+    public String toString(){
+        return "AubotV2 (bot de Lucas)";
+    }
+
+    //-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Extrêmement utile pour connaitre la force des dés du bot, lui permet de savoir quelles stratégies
+     * il devra adopter.
+     * @return un tableau des ressources moyennes gagnées par lancé par le dé dans l'ordre suivant: or/soleil/lune/pdg
+     */
+    private void statsDe() {
+        int or = 0;
+        int lune = 0;
+        int soleil = 0;
+        if (!desComplet)
+            for (De de : getDes())
+                for (Face face : de.getFaces())
+                    for (Ressource[] ressources : face.getRessource())
+                        for (Ressource ressource : ressources) {
+                            if (ressource instanceof Or)
+                                or += ressource.getQuantite();
+                            if (ressource instanceof Soleil)
+                                soleil += ressource.getQuantite();
+                            if (ressource instanceof Lune)
+                                lune += ressource.getQuantite();
+                        }
+        //System.out.println("or: " + or + "\nsoleil:" + soleil + "\nlune: " + lune);
+        //System.out.println("\n\n\n");
+        if (or >= orDe && lune >= luneDe && soleil >= soleilDe)
+            desComplet = true;
+    }
+
+    private int getPosFace1Or(int numDe){
+        for (int i = 0; i != 6; i++) {
+            if (getDe(numDe).getFace(i).getRessource().length == 1)
+                if (getDe(numDe).getFace(i).getRessource()[0][0] instanceof Or && getDe(numDe).getFace(i).getRessource()[0][0].getQuantite() == 1)
+                    return i;
+        }
+        return -1;
+    }
+
+    private Bassin trouveBassinCout(List<Bassin> bassins, int cout, String typeRessource){
+        if (typeRessource.equals("Or")||typeRessource.equals("Tout")) {
+            for (Bassin bassin : bassins)
+                if (bassin.getCout() == cout)
+                    for (Face face : bassin.getFaces())
+                        for (Ressource[] ressources : face.getRessource())
+                            for (Ressource ressource : ressources)
+                                if (ressource instanceof Or)
+                                    return bassin;
+        }
+        if (typeRessource.equals("Soleil")||typeRessource.equals("Tout")) {
+            for (Bassin bassin : bassins)
+                if (bassin.getCout() == cout)
+                    for (Face face : bassin.getFaces())
+                        for (Ressource[] ressources : face.getRessource())
+                            for (Ressource ressource : ressources)
+                                if (ressource instanceof Soleil)
+                                    return bassin;
+        }
+        if (typeRessource.equals("Lune")||typeRessource.equals("Tout")) {
+            for (Bassin bassin : bassins)
+                if (bassin.getCout() == cout)
+                    for (Face face : bassin.getFaces())
+                        for (Ressource[] ressources : face.getRessource())
+                            for (Ressource ressource : ressources)
+                                if (ressource instanceof Lune)
+                                    return bassin;
+        }
+        return null;
+    }
+
+    private int nombreCartePossedee(Carte.Noms nom){
+        int compte = 0;
+        for (Carte carte: getCartes())
+            if (carte.getNom() == nom)
+                compte++;
+        return compte;
+    }
+
+    private void ressourceManquante(){
+        orManquant = getMaxOr() - getOr();
+        luneManquant = getMaxLune() - getLune();
+        soleilManquant = getMaxSoleil() - getSoleil();
+    }
+}
