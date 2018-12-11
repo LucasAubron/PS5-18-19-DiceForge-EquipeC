047ab38052a6d93121e6ce1ad9b8c73d804e569b
Lucas Aubron <lucas.aubron@etu.unice.fr>
2018-12-11 21:23:30 +0100

    envoi final

--- src/main/java/diceForge/Main/Main.java
+++ src/main/java/diceForge/Main/Main.java
@@ -9,7 +9,7 @@ public class Main {
             //insérer dans le tableau (en deuxième argument lors de l'initialisation) entre 2 et 4 Enum de bot
             //Bot dispo dans l'ordre de force: ZizaBot, randomBot, NidoBot EasyBot, NidoBotV2, AubronBotV2
             boolean modeVerbeux = false; //true si on veut une seule partie détaillée, false si on veut 1000 parties avec stats
-            Coordinateur coordinateur1 = new Coordinateur(modeVerbeux, new Joueur.Bot[]{Joueur.Bot.AubronBotV2, Joueur.Bot.NidoBot});
+            Coordinateur coordinateur1 = new Coordinateur(modeVerbeux, new Joueur.Bot[]{Joueur.Bot.AubronBotV2, Joueur.Bot.RandomBot});
             System.out.println(coordinateur1.getAffichage() + "\n\n");
             Coordinateur coordinateur2 = new Coordinateur(modeVerbeux, new Joueur.Bot[]{Joueur.Bot.AubronBotV2, Joueur.Bot.AubronBotV2});
             System.out.println(coordinateur2.getAffichage());
